97257a66055d108745a6ade5b39d97ef44572cd1
Sam Eureka <sameureka@me.com>
2020-05-18 12:08:29 -0700

    added the S to HTTP

--- src/index.js
+++ src/index.js
@@ -1,62 +1,58 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import md5 from 'md5';
-import debounce from 'lodash.debounce';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import md5 from "md5";
+import debounce from "lodash.debounce";
 
-import styles from './components/Styles';
-
+import styles from "./components/Styles";
 
 class Hash extends Component {
-    constructor(){
-      super();
-      this.state = {
-        email: '',
-        grav: '',
-      }
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      grav: "",
+    };
   }
-  handleInput(e){
+  handleInput(e) {
     const input = e.target.value;
-      this.setState({
-        email: input,
-        grav: input === '' ? '' : md5(input),
-      })
-    }
+    this.setState({
+      email: input,
+      grav: input === "" ? "" : md5(input),
+    });
+  }
 
-  render(){
+  render() {
     return (
       <div>
-        <div style={styles.title}>
-          Gravatar?
-        </div>
+        <div style={styles.title}>Gravatar?</div>
         <input
           id="boom"
           type="text"
           placeholder="Enter Gravatar Id"
-          onChange={debounce(this.handleInput.bind(this), 789)}>
-        </input>
+          onChange={debounce(this.handleInput.bind(this), 789)}
+        ></input>
         <div>
-            <img src={`http://www.gravatar.com/avatar/${this.state.grav}?s=200`} />
-          <div style={styles.display}> Email: {' '}
-            <span style={styles.orange}>
-              {this.state.email}
-            </span>
+          <img
+            src={`https://www.gravatar.com/avatar/${this.state.grav}?s=200`}
+          />
+          <div style={styles.display}>
+            {" "}
+            Email: <span style={styles.orange}>{this.state.email}</span>
           </div>
-          <div style={styles.display}> MD5 Hash: {' '}
-              <span style={styles.orange}>
-                {this.state.grav}
-              </span>
+          <div style={styles.display}>
+            {" "}
+            MD5 Hash: <span style={styles.orange}>{this.state.grav}</span>
           </div>
-          <div style={styles.display}>{`Don't have a Gravatar? `}
+          <div style={styles.display}>
+            {`Don't have a Gravatar? `}
             <div>
-              <a href="http://en.gravatar.com">Click here!</a>
+              <a href="https://en.gravatar.com">Click here!</a>
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-
-
-ReactDOM.render(<Hash />, document.getElementById('app'));
+ReactDOM.render(<Hash />, document.getElementById("app"));
